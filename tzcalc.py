40f0723d321e2719da86863f04c40eb8587bc075
austendvds-boop <austen.dvds@gmail.com>
2026-02-03 12:43:19 -0700

    Wipe for fresh start

--- tzcalc.py
+++
@@ -1,8 +0,0 @@
-import datetime
-from datetime import timezone
-
-# 2026-02-14 07:00 MST = UTC-7
-# Represent as UTC timestamp
-
-dt = datetime.datetime(2026, 2, 14, 14, 0, 0, tzinfo=timezone.utc)
-print(int(dt.timestamp() * 1000))
